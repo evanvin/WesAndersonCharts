d3a14bca99280f44f4c7fe5c853227094211e6a8
Evan Vinciguerra <evanv511@gmail.com>
2023-06-25 17:47:41 -0400

    experimenting

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,16 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <ul v-for="actor in actors" :key="actor[0]">
+      <li>
+        {{ actor[0] }}
+        <ol>
+          <li v-for="role in actor[1]" :key="role['role']">
+            {{ role['role'] }}
+          </li>
+        </ol>
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -13,3 +23,23 @@
   }
 }
 </style>
+
+<script>
+import rolesData from '../../data/roles.json'
+
+const as = rolesData['actors']
+console.log(as)
+const actors = Object.entries(as)
+  .sort(([, a], [, b]) => b.length - a.length)
+  .slice(0, 10)
+
+console.log(actors)
+
+export default {
+  data() {
+    return {
+      actors
+    }
+  }
+}
+</script>

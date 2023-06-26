49d4ac381c1a057ed6485bd28ef15bb5125f242e
Evan Vinciguerra <evanv511@gmail.com>
2023-06-26 14:26:14 -0400

    deploy 4

--- deploy.sh
+++ deploy.sh
@@ -9,5 +9,5 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:username/hello-world.git master:gh-pages
+git push -f git@github.com:evanvin/WesAndersonCharts.git master:gh-pages
 cd -
